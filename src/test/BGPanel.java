829f5f7ddf8f8d01a4e827e9f11681cb100afc2a
Pradeepkumar Kannan <pradeep.tck@gmail.com>
2011-06-05 21:46:56 +0530

    Test panel to test slope of a line

---
+++ src/test/BGPanel.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+
+import javax.swing.JPanel;
+
+import util.Util;
+
+public class BGPanel extends JPanel {
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setColor(Color.RED);
+		g2d.setStroke(new BasicStroke(1));
+		g2d.drawOval(209, 111, 10, 10);
+		g2d.drawOval(589, 375, 10, 10);
+		
+		Point startPoint = new Point(209, 111);
+		Point endPoint = new Point(589, 375);
+		Double slope = ((endPoint.getX() - startPoint.getX()) / (endPoint.getY() - startPoint.getY()));
+		int count = 0;
+		while(count < 20){
+			Point next = Util.getNextPoint(startPoint, endPoint, 20, slope);
+			
+			g2d.setColor(Color.BLACK);
+			g2d.setStroke(new BasicStroke(1));
+			g2d.drawOval((int)next.getX(), (int)next.getY(), 10, 10);
+			
+			startPoint = next;
+			count++;
+		}
+
+	}
+
+}
